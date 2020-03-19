4a15e7f10f5e93f9b442b155b88ba326efd733db
Dmitry Frolov <dmitsf@gmail.com>
2020-03-19 14:11:05 +0300

    relevance calculation added

--- examples/corelevance_matrix.py
+++ examples/corelevance_matrix.py
@@ -26,14 +26,30 @@ def make_substrings(tokens, k=4):
 def get_corelevance_matrix(texts):
 
     matrix = np.empty((0, len(texts)), float)
-    prepared_tokens = [clear_text(t) for t in texts]
-    prepared_texts = [' '.join(t) for t in prepared_tokens]
-    
-    for text_tokens in prepared_tokens:
+    prepared_text_tokens = [clear_text(t) for t in texts]
+    prepared_texts = [' '.join(t) for t in prepared_text_tokens]
+
+    for text_tokens in prepared_text_tokens:
         ast = base.AST.get_ast(list(make_substrings(text_tokens)))
         row = np.array([ast.score(t) for t in prepared_texts])
         matrix = np.append(matrix, [row], axis=0)
-        
+
+    return matrix
+
+
+def get_relevance_matrix(texts, strings):
+
+    matrix = np.empty((0, len(strings)), float)
+    prepared_text_tokens = [clear_text(t) for t in texts]
+
+    prepared_string_tokens = [clear_text(s) for s in strings]    
+    prepared_strings = [' '.join(t) for t in prepared_string_tokens]
+
+    for text_tokens in prepared_text_tokens:
+        ast = base.AST.get_ast(list(make_substrings(text_tokens)))
+        row = np.array([ast.score(s) for s in prepared_strings])
+        matrix = np.append(matrix, [row], axis=0)
+
     return matrix
 
 
@@ -43,12 +59,24 @@ def save_matrix(matrix):
 
 if __name__ == "__main__":
 
+    # Corelevance matrix:
     texts = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
              "qui dolorem ipsum, quia dolor sit, amet!",
              "lorem ipsum"]
 
-    matrix = get_corelevance_matrix(texts)
+    corelevance_matrix = get_corelevance_matrix(texts)
+    print("Corelevance mairix:")
+    print(corelevance_matrix)
 
-    print(matrix)
+    # Saving
+    save_matrix(corelevance_matrix)
 
-    save_matrix(p)
+    # Relevance between the texts and strings:
+    strings = ["Loramet, ipsum",
+               "sit dolor amet r 6565?",
+               "il lorem",
+               "ip sum3"]
+
+    relevance_matrix = get_relevance_matrix(texts, strings)
+    print("Relevance mairix:")
+    print(relevance_matrix)
